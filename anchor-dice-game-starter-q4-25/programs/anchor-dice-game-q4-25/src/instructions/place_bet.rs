e315eb7b83949566803a1434918e7af0d419f2f5
saatvik-10 <saatvikmadan529@gmail.com>
2025-11-03 23:49:57 +0530

    completed anchor logic

--- anchor-dice-game-starter-q4-25/programs/anchor-dice-game-q4-25/src/instructions/place_bet.rs
+++ anchor-dice-game-starter-q4-25/programs/anchor-dice-game-q4-25/src/instructions/place_bet.rs
@@ -1,4 +1,7 @@
-use anchor_lang::{prelude::*, system_program::{Transfer, transfer}};
+use anchor_lang::{
+    prelude::*,
+    system_program::{transfer, Transfer},
+};
 
 use crate::state::Bet;
 
@@ -7,34 +10,44 @@ use crate::state::Bet;
 pub struct PlaceBet<'info> {
     #[account(mut)]
     pub player: Signer<'info>,
+
     ///CHECK: This is safe
     pub house: UncheckedAccount<'info>,
+    
     #[account(
         mut,
         seeds = [b"vault", house.key().as_ref()],
         bump
     )]
     pub vault: SystemAccount<'info>,
+
     #[account(
         init,
         payer = player,
-        space = Bet::Discriminator.len() + Bet::INIT_SPACE,
+        space = Bet::DISCRIMINATOR.len() + Bet::INIT_SPACE,
         seeds = [b"bet", vault.key().as_ref(), seed.to_le_bytes().as_ref()],
         bump
     )]
     pub bet: Account<'info, Bet>,
-    pub system_program: Program<'info, System>
+
+    pub system_program: Program<'info, System>,
 }
 
 impl<'info> PlaceBet<'info> {
-    pub fn create_bet(&mut self, bumps: &PlaceBetBumps, seed: u128, roll: u8, amount: u64) -> Result<()> {
-        self.bet.set_inner(Bet{
-            slot : Clock::get()?.slot,
+    pub fn create_bet(
+        &mut self,
+        bumps: &PlaceBetBumps,
+        seed: u128,
+        roll: u8,
+        amount: u64,
+    ) -> Result<()> {
+        self.bet.set_inner(Bet {
+            slot: Clock::get()?.slot,
             player: self.player.key(),
             seed,
             roll,
             amount,
-            bump : bumps.bet,
+            bump: bumps.bet,
         });
         Ok(())
     }
@@ -42,13 +55,10 @@ impl<'info> PlaceBet<'info> {
     pub fn deposit(&mut self, amount: u64) -> Result<()> {
         let accounts = Transfer {
             from: self.player.to_account_info(),
-            to: self.vault.to_account_info()
+            to: self.vault.to_account_info(),
         };
 
-        let ctx = CpiContext::new(
-            self.system_program.to_account_info(),
-            accounts
-        );
+        let ctx = CpiContext::new(self.system_program.to_account_info(), accounts);
         transfer(ctx, amount)
     }
 }
--- anchor-dice-game-starter-q4-25/programs/anchor-dice-game-q4-25/src/instructions/refund_bet.rs
+++ anchor-dice-game-starter-q4-25/programs/anchor-dice-game-q4-25/src/instructions/refund_bet.rs
@@ -9,14 +9,17 @@ use crate::{errors::DiceError, state::Bet};
 pub struct RefundBet<'info> {
     #[account(mut)]
     pub player: Signer<'info>,
+
     ///CHECK: This is safe
     pub house: UncheckedAccount<'info>,
+
     #[account(
         mut,
         seeds = [b"vault", house.key().as_ref()],
         bump
     )]
     pub vault: SystemAccount<'info>,
+
     #[account(
         mut,
         close = player,
@@ -24,6 +27,7 @@ pub struct RefundBet<'info> {
         bump = bet.bump
     )]
     pub bet: Account<'info, Bet>,
+
     pub system_program: Program<'info, System>,
 }
 
