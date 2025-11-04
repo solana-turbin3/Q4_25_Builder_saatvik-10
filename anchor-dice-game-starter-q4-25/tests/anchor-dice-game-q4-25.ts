92a3a4d5dbe871b83a00c53024757b17712abbd3
saatvik-10 <saatvikmadan529@gmail.com>
2025-11-04 17:22:42 +0530

    test

--- anchor-dice-game-starter-q4-25/tests/anchor-dice-game-q4-25.ts
+++ anchor-dice-game-starter-q4-25/tests/anchor-dice-game-q4-25.ts
@@ -1,16 +1,23 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import anchor, { Program } from "@coral-xyz/anchor";
 import { AnchorDiceGameQ425 } from "../target/types/anchor_dice_game_q4_25";
+import {
+    Ed25519Program,
+    Keypair,
+    sendAndConfirmTransaction,
+    Transaction,
+    SystemProgram,
+    SYSVAR_INSTRUCTIONS_PUBKEY
+} from "@solana/web3.js"
 
 describe("anchor-dice-game-q4-25", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    const clusterProvider = anchor.AnchorProvider.env();
+    anchor.setProvider(clusterProvider);
 
-  const program = anchor.workspace.anchorDiceGameQ425 as Program<AnchorDiceGameQ425>;
+    const prgrm = anchor.workspace.DiceGame as Program<AnchorDiceGameQ425>
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+    const house = anchor.web3.Keypair.generate();
+    const place = anchor.web3.Keypair.generate();
+
+    let vaultPda: anchor.web3.PublicKey;
+
+})
