1a1f4a958d2554ff25e7c390cf427f8a0a77cbdf
saatvik-10 <saatvikmadan529@gmail.com>
2025-11-07 13:33:11 +0530

    input err check

--- Capstone/tap-shield/programs/tap-shield/src/instructions/initialize_faucet.rs
+++ Capstone/tap-shield/programs/tap-shield/src/instructions/initialize_faucet.rs
@@ -22,16 +22,21 @@ pub struct InitializeFaucet<'info> {
 impl<'info> InitializeFaucet<'info> {
     pub fn initialize_faucet(&mut self, name: String) -> Result<()> {
         require!(name.len() <= 32, TapShieldErr::FaucetNameTooLong);
+        require!(!name.is_empty(), TapShieldErr::InvalidInput);
 
         let faucet_registry = &mut self.faucet_registry;
         let clock = Clock::get()?;
 
         faucet_registry.operator = self.operator.key();
-        faucet_registry.name = name;
+        faucet_registry.name = name.clone();
         faucet_registry.total_claims = 0;
         faucet_registry.created_at = clock.unix_timestamp;
 
-        msg!("FAUCET: {} REGISTERED SUCCESSFULLY!", faucet_registry.name);
+        msg!(
+            "FAUCET: {} REGISTERED SUCCESSFULLY BY {} !",
+            name,
+            self.operator.key()
+        );
 
         Ok(())
     }
