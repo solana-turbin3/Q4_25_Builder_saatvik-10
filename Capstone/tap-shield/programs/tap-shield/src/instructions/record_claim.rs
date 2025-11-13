496899e7b00b47c54754a3af150207048ca3fec4
saatvik-10 <saatvikmadan529@gmail.com>
2025-11-13 14:53:43 +0530

    merged eligibility and claim

--- Capstone/tap-shield/programs/tap-shield/src/instructions/record_claim.rs
+++ Capstone/tap-shield/programs/tap-shield/src/instructions/record_claim.rs
@@ -30,16 +30,50 @@ pub struct RecordClaim<'info> {
     )]
     pub claim_record: Account<'info, ClaimRecord>,
 
+    #[account(
+        mut,
+        seeds = [b"claim", claimer.key().as_ref(), faucet_registry.key().as_ref(), &faucet_registry.total_claims.saturating_sub(1).to_le_bytes()],
+        bump
+    )]
+    pub last_claim_record: Option<Account<'info, ClaimRecord>>,
+
     pub system_program: Program<'info, System>,
 }
 
 impl<'info> RecordClaim<'info> {
-    pub fn record_claim(&mut self, claimer_pubkey: Pubkey, amount: u64) -> Result<()> {
+    pub fn record_claim(
+        &mut self,
+        claimer_pubkey: Pubkey,
+        amount: u64,
+        cooldown_second: i64,
+    ) -> Result<()> {
         let claim = &mut self.claim_record;
         let faucet_registry = &mut self.faucet_registry;
         let clock = Clock::get()?;
+        let curr_time = clock.unix_timestamp;
 
         require!(amount > 0, TapShieldErr::InvalidInput);
+        require!(cooldown_second > 0, TapShieldErr::InvalidCooldown);
+        require!(
+            claimer_pubkey == self.claimer.key(),
+            TapShieldErr::InvalidClaimer
+        );
+
+        if let Some(last_claim) = &self.last_claim_record {
+            let time_since_last_claim = curr_time - last_claim.timestamp;
+
+            require!(
+                time_since_last_claim >= cooldown_second,
+                TapShieldErr::ClaimTooRecent
+            );
+
+            msg!(
+                "Cooldown has been passed. {} seconds since last claim",
+                time_since_last_claim
+            );
+        } else {
+            msg!("First time claimer ? ELIGIBLE");
+        }
 
         claim.claimer = claimer_pubkey;
         claim.faucet_id = faucet_registry.key();
