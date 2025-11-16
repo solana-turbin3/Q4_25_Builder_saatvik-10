26a19f510e50f853a5cc91f4db62e7b9cf0d5daa
saatvik-10 <saatvikmadan529@gmail.com>
2025-11-16 19:51:40 +0530

    added record claim test

--- Capstone/tap-shield/tests/tap-shield.ts
+++ Capstone/tap-shield/tests/tap-shield.ts
@@ -14,7 +14,8 @@ describe("tap-shield", () => {
   let claimer: anchor.web3.Keypair
   let faucetRegistryPda: PublicKey
   let newFaucetRegistryPdA: PublicKey
-  let claimRecordPda: anchor.web3.Keypair
+  let claimRecordPda: PublicKey
+  let nextClaimPda: PublicKey
 
   const TEST_FAUCET_NAME = "TEST FAUCET"
   const CLAIM_AMOUNT = new anchor.BN(4000000)
@@ -29,6 +30,13 @@ describe("tap-shield", () => {
     const [pda] = PublicKey.findProgramAddressSync(
       [Buffer.from("faucet"), pubKey.toBuffer()],
       program.programId
+    )
+    return pda
+  }
+
+  function getClaimRecordPda(claimerPubKey: PublicKey, faucetPubKey: PublicKey, claimIndex: anchor.BN): PublicKey {
+    const [pda] = PublicKey.findProgramAddressSync(
+      [Buffer.from("claim"), claimerPubKey.toBuffer(), faucetPubKey.toBuffer(), claimIndex.toArrayLike(Buffer, "le", 8)], program.programId
     )
     return pda
   }
@@ -63,7 +71,7 @@ describe("tap-shield", () => {
 
       expect(faucetAccount.operator.toString()).to.equal(operator.publicKey.toString())
       expect(faucetAccount.name).to.equal(TEST_FAUCET_NAME)
-      expect(faucetAccount.totalClaims).to.equal(0)
+      expect(faucetAccount.totalClaims.toNumber()).to.equal(0)
       expect(faucetAccount.createdAt.toNumber()).to.be.greaterThan(0)
     })
 
@@ -79,12 +87,12 @@ describe("tap-shield", () => {
 
         expect.fail("Should throw FaucetAlreadyExists err")
       } catch (err) {
-        console.log(err)
+        console.log("FaucetAlreadyExists err")
       }
     })
 
     it("Should fail if name > 32 chars", async () => {
-      const lName = "asd".repeat(40)
+      const TEST_FAUCET_LONG_NAME = "asd".repeat(40)
       const newOperator = anchor.web3.Keypair.generate()
 
       await airdrop(newOperator.publicKey, 5)
@@ -98,11 +106,68 @@ describe("tap-shield", () => {
           systemProgram: anchor.web3.SystemProgram.programId,
         }
 
-        await program.methods.initializeFaucet(TEST_FAUCET_NAME).accounts(accounts).signers([operator]).rpc()
+        await program.methods.initializeFaucet(TEST_FAUCET_LONG_NAME).accounts(accounts).signers([operator]).rpc()
 
         expect.fail("Should throw FaucetNameTooLong err")
       } catch (err) {
-        console.log(err)
+        console.log("FaucetNameTooLong err")
+      }
+    })
+  })
+
+  describe("Claim Record", () => {
+    before(async () => {
+      const faucetAccount = await program.account.faucetRegistry.fetch(faucetRegistryPda)
+
+      claimRecordPda = getClaimRecordPda(claimer.publicKey, faucetRegistryPda, faucetAccount.totalClaims)
+    })
+
+    it("Should successfully record a claim", async () => {
+      const accounts = {
+        operator: operator.publicKey,
+        claimer: claimer.publicKey,
+        faucetRegistry: faucetRegistryPda,
+        claimRecord: claimRecordPda,
+        lastClaimRecord: null,
+        systemProgram: anchor.web3.SystemProgram.programId
+      }
+
+      const txn = await program.methods.recordClaim(claimer.publicKey, CLAIM_AMOUNT, COOLDOWN_SECONDS).accounts(accounts).signers([operator]).rpc()
+
+      console.log("Record claim txn: ", txn)
+
+      const claimAccount = await program.account.claimRecord.fetch(claimRecordPda)
+
+      expect(claimAccount.claimer.toString()).to.equal(claimer.publicKey.toString())
+      expect(claimAccount.faucetId.toString()).to.equal(faucetRegistryPda.toString())
+      expect(claimAccount.amount.toNumber()).to.equal(CLAIM_AMOUNT.toNumber())
+      expect(claimAccount.timestamp.toNumber()).to.greaterThan(0)
+
+      const faucetAccount = await program.account.faucetRegistry.fetch(faucetRegistryPda)
+
+      expect(faucetAccount.totalClaims.toNumber()).to.equal(1)
+    })
+
+    it("Should fail when claiming to soon", async () => {
+      const faucetAccount = await program.account.faucetRegistry.fetch(faucetRegistryPda)
+
+      nextClaimPda = getClaimRecordPda(claimer.publicKey, faucetRegistryPda, faucetAccount.totalClaims)
+
+      const accounts = {
+        operator: operator.publicKey,
+        claimer: claimer.publicKey,
+        faucetRegistry: faucetRegistryPda,
+        claimRecord: nextClaimPda,
+        lastClaimRecord: claimRecordPda,
+        systemProgram: anchor.web3.SystemProgram.programId
+      }
+
+      try {
+        await program.methods.recordClaim(claimer.publicKey, CLAIM_AMOUNT, COOLDOWN_SECONDS).accounts(accounts).signers([operator]).rpc()
+        expect.fail("Should throw ClaimTooRecent err")
+      }
+      catch (err) {
+        console.log("ClaimTooRecent err");
       }
     })
   })
