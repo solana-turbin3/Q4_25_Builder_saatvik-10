a69d5d6d39bc4f888ce869847c7a74b623bccdfb
saatvik-10 <saatvikmadan529@gmail.com>
2025-11-19 08:12:17 +0530

    hydration suppressing

--- Capstone/tap-shield/demo-faucet-prgm/app/layout.tsx
+++ Capstone/tap-shield/demo-faucet-prgm/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { WalletConnectorProvider } from '@/app/components/WalletProvider';
+import { WalletConnectorProvider } from '@/components/WalletProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning={true}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
